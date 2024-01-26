ef890b459d8c21066413a6ca4348418dcb73ca58
AdnanRohmatKurniansah <atnandimas@gmail.com>
2024-01-26 21:02:54 +0700

    passthrough

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('product')
 //prefix
 export class ProductController {
   @Get()
-  findAll(): string {
-    return 'This action returns all product';
+  findAll(@Res() res: Response) {
+    res.json({
+      name: 'oreo',
+    });
   }
   @Get('create')
-  create(): string {
+  create(@Res({ passthrough: true }) res: Response): string {
+    res.cookie('name', 'adnan');
     return 'This is route create';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} product`;
   }
 }
