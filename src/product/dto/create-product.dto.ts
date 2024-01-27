8191af3c84b0e8c75ec43af385cff3ac2c61f372
AdnanRohmatKurniansah <atnandimas@gmail.com>
2024-01-27 15:21:21 +0700

    validation pipe

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsNumber()
+  @IsNotEmpty()
   id: number;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
+
+  @IsNotEmpty()
+  @IsString()
   desc: string;
 }
